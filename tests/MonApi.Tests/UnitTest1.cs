a2646f913c5d2cf1fa085af8797aced71fa618d9
Mel-redoc <armel.ran48@gmail.com>
2026-07-11 03:16:22 +0200

    Initial commit: API ASP.NET Core avec tests

---
+++ tests/MonApi.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MonApi.Tests;
+
+public class HealthEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public HealthEndpointTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Health_Endpoint_Returns_Ok()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/health");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task WeatherForecast_Endpoint_Returns_Ok()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/weatherforecast");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
